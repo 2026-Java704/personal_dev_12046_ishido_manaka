c30ee821c664f27c3698a40ac3bb942243f564d8
student <student@FJ-1025>
2026-05-19 12:04:17 +0900

    作成

---
+++ src/main/java/com/example/demo/PersonalDev12046IshidoManakaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDev12046IshidoManakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDev12046IshidoManakaApplication.class, args);
+	}
+
+}
